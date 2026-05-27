33170e85a5000c67fa52539187390f3c88dad995
Sandrine Ceuppens <sandrineceuppens@icloud.com>
2026-05-27 15:42:47 +0200

    Revert "Remove duplicate content from sub-pages (tiktok-ricoh-europe, instagram-pentax-europe)"
    This reverts commit 03bd64a5ba9af68cd0a8d211d5499c954e9cfaf1.

--- app/[lang]/seen/instagram-pentax-europe/page.tsx
+++ app/[lang]/seen/instagram-pentax-europe/page.tsx
@@ -38,7 +38,48 @@ interface GalleryItem {
   brand?: string;
 }
 
-const GALLERY_ITEMS: GalleryItem[] = [];
+const GALLERY_ITEMS: GalleryItem[] = [
+  {
+    id: "pentax-sicily-1",
+    type: "video",
+    src: "/videos/pentax-sicily-1.mp4",
+    thumbnail: "/images/gallery/pentax-sicily-1.jpg",
+    link: "https://www.instagram.com/reel/DYrwp_bNhQa/",
+    brand: "PENTAX EUROPE",
+  },
+  {
+    id: "pentax-sicily-2",
+    type: "video",
+    src: "/videos/pentax-sicily-2.mp4",
+    thumbnail: "/images/gallery/pentax-sicily-2.jpg",
+    link: "https://www.instagram.com/reel/DYkCMzYtIVz/",
+    brand: "PENTAX EUROPE",
+  },
+  {
+    id: "pentax-sicily-3",
+    type: "video",
+    src: "/videos/pentax-sicily-3.mp4",
+    thumbnail: "/images/gallery/pentax-sicily-3.jpg",
+    link: "https://www.instagram.com/reel/DYhdaEsNqxd/",
+    brand: "PENTAX EUROPE",
+  },
+  {
+    id: "pentax-sicily-4",
+    type: "video",
+    src: "/videos/pentax-sicily-4.mp4",
+    thumbnail: "/images/gallery/pentax-sicily-4.jpg",
+    link: "https://www.instagram.com/reel/DYSAsOENsPh/",
+    brand: "PENTAX EUROPE",
+  },
+  {
+    id: "pentax-sicily-5",
+    type: "video",
+    src: "/videos/pentax-sicily-5.mp4",
+    thumbnail: "/images/gallery/pentax-sicily-5.jpg",
+    link: "https://www.instagram.com/reel/DYPb5uOt4ju/",
+    brand: "PENTAX EUROPE",
+  },
+];
 
 export default async function Page({ params }: Props) {
   const { lang } = await params;
--- app/[lang]/seen/tiktok-ricoh-europe/page.tsx
+++ app/[lang]/seen/tiktok-ricoh-europe/page.tsx
@@ -38,7 +38,24 @@ interface GalleryItem {
   brand?: string;
 }
 
-const GALLERY_ITEMS: GalleryItem[] = [];
+const GALLERY_ITEMS: GalleryItem[] = [
+  {
+    id: "ricoh-tiktok-1",
+    type: "video",
+    src: "/videos/ricoh-tiktok-1.mp4",
+    thumbnail: "/images/gallery/ricoh-tiktok-1.jpg",
+    link: "https://www.tiktok.com/@ricoh_gr/video/7583019198180314390",
+    brand: "RICOH EUROPE",
+  },
+  {
+    id: "ricoh-tiktok-2",
+    type: "video",
+    src: "/videos/ricoh-tiktok-2.mp4",
+    thumbnail: "/images/gallery/ricoh-tiktok-2.jpg",
+    link: "https://www.tiktok.com/@ricoh_gr/video/7575548335138917654",
+    brand: "RICOH EUROPE",
+  },
+];
 
 export default async function Page({ params }: Props) {
   const { lang } = await params;
